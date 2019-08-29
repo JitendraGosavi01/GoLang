3047eefbd1849d2614cc99294ed5f48d5111cb70
JITENDRA RAJENDRA GOSAVI <JG00601363@TECHMAHINDRA.COM>
2019-08-29 17:07:03 +0530

    unstructured folder removed

--- Go Controls/for/for.go
+++
@@ -1,8 +0,0 @@
-package main
-import "fmt"
-func main(){
-   
-	for a:=1; a<11;a++ {
-		fmt.Println("value of a is :", a);
-	}
-}
--- Go Functions/func-with-return.go
+++
@@ -1,9 +0,0 @@
-package main
-import "fmt"
-func test()string {
-	return "Hello GoLang";
-}
-func main(){
-	x:= test();
-	fmt.Print(x);
-}
